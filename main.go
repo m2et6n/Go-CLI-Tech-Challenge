086965711617fd82c0b669362a33f74025312fe2
Mike Thomason <mthomason@captechconsulting.com>
2025-02-04 19:41:11 -0500

    Added create and delete

--- main.go
+++ main.go
@@ -1,3 +1,150 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	ctx := context.Background()
+	fs := FS{}
+	if err := run(ctx, fs, os.Args, os.Stdout); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context, fs fileSystem, args []string, stdout io.Writer) error {
+
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+
+	defer cancel()
+
+	if len(args) < 2 {
+		return fmt.Errorf("Invalid command.\nUsage: go run main.go [command] [args]")
+	}
+	command := args[1]
+
+	switch command {
+	case "list":
+		return list(fs, stdout, args)
+	case "create":
+		return writeFile(fs, stdout, args)
+	case "delete":
+		return deleteFile(fs, stdout, args)
+	default:
+		return fmt.Errorf("Unknown command:%s\nUsage: go run main.go [list|create|delete|move] [args]", command)
+	}
+
+	return nil
+}
+
+type FS struct{}
+
+/*
+Functions for implementing the list command
+*/
+
+// Main list() function
+func list(rfs readFileSystem, stdout io.Writer, args []string) error {
+	if len(args) < 3 {
+		return fmt.Errorf("Invalid arguments.\nUsage: go run main.go list [directory]")
+	}
+
+	dir := args[2]
+	files, err := rfs.ReadDir(dir)
+	if err != nil {
+		return fmt.Errorf("Error reading directory: %s", err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			fmt.Fprint(stdout, file.Name()+"/\n")
+		} else {
+			fmt.Fprint(stdout, file.Name()+"\n")
+		}
+	}
+
+	return nil
+}
+
+type readFileSystem interface {
+	ReadDir(name string) ([]os.DirEntry, error)
+}
+
+func (f FS) ReadDir(name string) ([]os.DirEntry, error) {
+	return os.ReadDir(name)
+}
+
+/*
+Functions for implementing the create command
+*/
+
+// Main create() function
+func writeFile(fw fileWriter, stdout io.Writer, args []string) error {
+	if len(args) < 3 {
+		return fmt.Errorf("Invalid arguments.\nUsage: go run main.go create [file-path]")
+	}
+
+	fileName := args[2]
+	fileHandle, err := fw.WriteFile(fileName)
+	if err != nil {
+		return fmt.Errorf("Error creating file: %s", err)
+	}
+
+	if fileHandle != nil {
+		fmt.Fprintf(stdout, "File created: %s\n", fileName)
+	}
+
+	return nil
+}
+
+type fileWriter interface {
+	WriteFile(filePath string) (*os.File, error)
+}
+
+type FW struct{}
+
+func (f FS) WriteFile(filePath string) (*os.File, error) {
+	return os.Create(filePath)
+}
+
+/*
+Functions for implementing the delete command
+*/
+
+// Main delete() function
+func deleteFile(fd fileDeleter, stdout io.Writer, args []string) error {
+	if len(args) < 3 {
+		return fmt.Errorf("Invalid arguments.\nUsage: go run main.go delete [file-path]")
+	}
+
+	fileName := args[2]
+	err := fd.DeleteFile(fileName)
+	if err != nil {
+		return fmt.Errorf("Error deleting file: %s", err)
+	}
+
+	fmt.Fprintf(stdout, "File deleted: %s\n", fileName)
+
+	return nil
+}
+
+type fileDeleter interface {
+	DeleteFile(filePath string) error
+}
+
+type FD struct{}
+
+func (f FS) DeleteFile(filePath string) error {
+	return os.Remove(filePath)
+}
+
+type fileSystem interface {
+	readFileSystem
+	fileWriter
+	fileDeleter
+}
--- main_test.go
+++ main_test.go
@@ -1,1 +1,174 @@
 package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+/*
+Tests for list command
+*/
+type mockRFS struct {
+	read func(name string) ([]os.DirEntry, error)
+}
+
+func (m mockRFS) ReadDir(name string) ([]os.DirEntry, error) {
+	return m.read(name)
+}
+
+func TestList(t *testing.T) {
+	t.Parallel()
+	type fields struct {
+		rfs  readFileSystem
+		args []string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    string
+		wantErr string
+	}{
+		{
+			name: "list happy path",
+			fields: fields{
+				rfs: mockRFS{
+					read: func(name string) ([]os.DirEntry, error) {
+						return []os.DirEntry{
+							mockDirEntry("attachments", true),
+							mockDirEntry("agendas", true),
+							mockDirEntry("sample_file.txt", false),
+						}, nil
+					},
+				},
+				args: []string{"main.go", "list", "meetings"},
+			},
+			want:    "attachments/\nagendas/\nsample_file.txt\n",
+			wantErr: "",
+		},
+		{
+			name: "list error reading directory",
+			fields: fields{
+				rfs: mockRFS{
+					read: func(name string) ([]os.DirEntry, error) {
+						return nil, fmt.Errorf("fake-error")
+					},
+				},
+				args: []string{"main.go", "list", "meetings"},
+			},
+			want:    "",
+			wantErr: "Error reading directory: fake-error",
+		},
+		{
+			name: "missing required list arguments",
+			fields: fields{
+				rfs: mockRFS{
+					read: func(name string) ([]os.DirEntry, error) {
+						return nil, fmt.Errorf("fake-error")
+					},
+				},
+				args: []string{"main.go", "list"},
+			},
+			want:    "",
+			wantErr: "Invalid arguments.\nUsage: go run main.go list [directory]",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			var bytes bytes.Buffer
+			err := list(tt.fields.rfs, &bytes, tt.fields.args)
+			if tt.wantErr == "" {
+				assert.NoError(t, err)
+			} else {
+				assert.Equal(t, tt.wantErr, err.Error())
+			}
+			got := bytes.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+/*
+Tests for create command
+*/
+type mockFW struct {
+	write func(filePath string) (*os.File, error)
+}
+
+func (m mockFW) WriteFile(filePath string) (*os.File, error) {
+	return m.write(filePath)
+}
+
+func TestCreate(t *testing.T) {
+	t.Parallel()
+	type fields struct {
+		fw   fileWriter
+		args []string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    string
+		wantErr string
+	}{
+		{
+			name: "create happy path",
+			fields: fields{
+				fw: mockFW{
+					write: func(filePath string) (*os.File, error) {
+						return &os.File{}, nil
+					},
+				},
+				args: []string{"main.go", "create", "test_file.txt"},
+			},
+			want:    "File created: test_file.txt\n",
+			wantErr: "",
+		},
+		{
+			name: "create error reading directory",
+			fields: fields{
+				fw: mockFW{
+					write: func(filePath string) (*os.File, error) {
+						return nil, fmt.Errorf("fake-error")
+					},
+				},
+				args: []string{"main.go", "create", "meetings"},
+			},
+			want:    "",
+			wantErr: "Error creating file: fake-error",
+		},
+		{
+			name: "missing required list arguments",
+			fields: fields{
+				fw: mockFW{
+					write: func(fileName string) (*os.File, error) {
+						return nil, fmt.Errorf("fake-error")
+					},
+				},
+				args: []string{"main.go", "create"},
+			},
+			want:    "",
+			wantErr: "Invalid arguments.\nUsage: go run main.go create [file-path]",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			var bytes bytes.Buffer
+			err := writeFile(tt.fields.fw, &bytes, tt.fields.args)
+			if tt.wantErr == "" {
+				assert.NoError(t, err)
+			} else {
+				assert.Equal(t, tt.wantErr, err.Error())
+			}
+			got := bytes.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
